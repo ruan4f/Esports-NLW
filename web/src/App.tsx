dac6352c53ef3dbbcddb0903856498c5bcae8aa2
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-09-13 22:49:49 -0300

    Tailwind configuration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,17 @@
+import './styles/main.css';
+
+import logoImg from './assets/logo-nlw-esports.svg';
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="logo" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+      </h1>
+    </div>
+  );
 }
 
 export default App
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+    "./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient':'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)'
+      }
+    },
+  },
+  plugins: [],
+}
