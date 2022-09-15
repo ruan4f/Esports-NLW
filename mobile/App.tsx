1875fb1565d59b59a927da539837ae56aac6af08
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-09-15 00:54:39 -0300

    Create splash screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-interface ButtonProps {
-    title: string;
-}
-
-function Button(props: ButtonProps){
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>      
-      
-      <Button title="Send 1"/>
-      <Button title="Send 2"/>
-      <Button title="Send 3"/>
-      <Button title="Hello World"/>
+    <Background >
 
-      <StatusBar style="auto" />
-    </View>
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },  
-});
