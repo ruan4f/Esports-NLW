ea0c121e05cbd51fb1cd1ae67ff3e932adb20245
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-09-15 01:37:31 -0300

    New components and screen
    GameCard, Loading, Heading Component and Home screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,34 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black, useFonts
+} from '@expo-google-fonts/inter';
+import { StatusBar } from 'react-native';
+
+
 import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoad] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+
+
   return (
     <Background >
-
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoad ? <Home /> : <Loading />}
     </Background>
   );
 }
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image, View } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <GameCard data={GAMES[0]}/>
+    </View>
+  );
+}
