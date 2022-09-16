5c5ddd2ec280564608859c394f4f3094dbae84d8
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-09-16 19:20:11 -0300

    Create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,24 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
-  return response.json([
-    {id: 1, name: 'Anuncio 1'},
-    {id: 2, name: 'Anuncio 2'},
-    {id: 3, name: 'Anuncio 3'},
-    {id: 4, name: 'Anuncio 4'},
-  ]);
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  const gameId = request.params.id;
+
+  return response.json([]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
