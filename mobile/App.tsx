df9e84acfa8472e7f049b547bdd0f3c44afdf204
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-10-05 21:15:27 -0300

    Notification

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,10 +6,11 @@ import {
 } from '@expo-google-fonts/inter';
 import { StatusBar } from 'react-native';
 
-
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+
+import './src/services/notificationConfigs';
 
 export default function App() {
   const [fontsLoad] = useFonts({
